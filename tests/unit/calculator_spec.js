3b0a25b28eaeb3d37588b080035ef9698efef5a7
peaky76 <robertjamespeacock@gmail.com>
2020-09-28 15:24:57 +0100

    unit tests passing

---
+++ tests/unit/calculator_spec.js
@@ -0,0 +1,65 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('can add two numbers', function() {
+    calculator.previousTotal = 1;
+    calculator.add(4);
+    assert.equal(5, calculator.runningTotal);
+  })
+
+  it('can subtract two numbers', function() {
+    calculator.previousTotal = 7
+    calculator.subtract(4)
+    assert.equal(3, calculator.runningTotal);
+  })
+
+  it('can multiply two numbers', function() {
+    calculator.previousTotal = 3
+    calculator.multiply(5)
+    assert.equal(15, calculator.runningTotal)
+  })
+
+  it('can divide two numbers', function() {
+    calculator.previousTotal = 21
+    calculator.divide(7)
+    assert.equal(3, calculator.runningTotal)
+  })
+
+  it('can concatenate multiple number button clicks', function() {
+    calculator.numberClick(2)
+    calculator.numberClick(5)
+    calculator.numberClick(2)
+    assert.equal(252, calculator.runningTotal)
+  })
+
+  it('can chain multiple operations together', function() {
+    calculator.numberClick(2)
+    calculator.operatorClick("+")
+    calculator.numberClick(8)
+    calculator.operatorClick("/")
+    calculator.numberClick(5)
+    calculator.operatorClick("=")
+    assert.equal(2, calculator.runningTotal)
+  })
+
+  it('can clear the running total', function() {
+    calculator.runningTotal = 10
+    calculator.clearClick()
+    calculator.add(2)
+    assert.equal(2, calculator.runningTotal)
+  }) 
+
+  // calculator.numberClick() - concatenate multiple number button clicks
+  // calculator.operatorClick() - chain multiple operations together
+  // calculator.clearClick() - clear the running total without affecting the calculation
+
+
+
+
+});
